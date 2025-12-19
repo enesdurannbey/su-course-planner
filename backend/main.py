3cf813ef122f2c07e36b819f9d3bb9f44864fc1e
enesdurannbey <enesduranbey@gmail.com>
2025-12-19 21:02:28 +0300

    fix: Relax CORS policy for PR previews

--- backend/main.py
+++ backend/main.py
@@ -33,8 +33,7 @@ app.add_middleware(
     allow_origins=[], 
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
-    allow_origin_regex="https://.*\.vercel\.app", 
+    allow_headers=["*"], 
 )
 
 app.add_middleware(
