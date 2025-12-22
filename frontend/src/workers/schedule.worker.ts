482d43bb8cbf51b0cbdd06461ba190fe51bd2725
enesdurannbey <enesduranbey@gmail.com>
2025-12-22 14:05:03 +0300

    added pin-section feature

--- frontend/src/workers/schedule.worker.ts
+++ frontend/src/workers/schedule.worker.ts
@@ -1,13 +1,14 @@
 import { runScheduler } from "../logic/scheduler";
 
 self.onmessage = (e: MessageEvent) => {
-  const { selected, courses, constraints } = e.data;
+  const { selected, courses, constraints, pinnedSections } = e.data;
 
   try {
-    const result = runScheduler(selected, courses, constraints);
+    const result = runScheduler(selected, courses, constraints, pinnedSections);
     
     self.postMessage({ status: "success", schedules: result });
   } catch (error) {
+    console.error("Worker Logic Error:", error);
     self.postMessage({ status: "error", error: error });
   }
 };
